868a4d92dee49ceddcb9d67a66e191977b98ed3f
Treasure <odetokuntreasure6@gmail.com>
2023-08-07 02:20:59 +0100

    css completed

--- js/app.js
+++ js/app.js
@@ -1,5 +1,20 @@
 "use strict";
+const openModal = document.querySelector('.btn-rules');
+const closeModal = document.querySelector('.close-modal')
 
+
+
+
+
+
+//event listeners
+openModal.addEventListener('click', function(){
+    document.querySelector('.modal-rules').style.display = 'block'
+})
+
+closeModal.addEventListener('click', function(){
+    document.querySelector('.modal-rules').style.display = 'none'
+})
 
 //getting the image location of each click on either rock,paper,scissors
 console.log(document.querySelector('.user').innerHTML)
